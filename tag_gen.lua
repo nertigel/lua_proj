4ef4045acac83b7dd844e90c4fac2555b50c9e86
Patrik Nertigel <nertigel14475@gmail.com>
2021-06-19 16:46:42 +0300

    tag_gen aka keyword tool tweak

--- tag_gen.lua
+++ tag_gen.lua
@@ -15,10 +15,9 @@ local v2 = {
     "Money",
 }
 
-local v3 = (function()
-    local _v1 = [[
-        keywords: 
-    ]]
+local v3 = (function(__v1)
+    local _v1 = [[keywords: 
+]]
     local _v2 = _v1
     for k, v in pairs(v1) do
         for _k, _v in pairs(v2) do
@@ -26,7 +25,7 @@ local v3 = (function()
             if (_v1 == _v2) then
                 _v1 = (_v1.._v3)
             else
-                _v1 = (_v1..", ".._v3)
+                _v1 = (_v1..", "..(__v1 and "\n" or "").._v3)
             end
         end
     end
@@ -34,4 +33,4 @@ local v3 = (function()
     print(_v1)
 end)
 
-v3()
+v3(false) --[[pass true for first argument in order to have new lines after each keyword]]
