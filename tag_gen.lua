937ac0e093145dc3cd5a5533a226bf4ef82c0872
Patrik Nertigel <nertigel14475@gmail.com>
2021-06-19 16:39:50 +0300

    added keyword gen
    a simple keyword generator for youtube videos

---
+++ tag_gen.lua
@@ -0,0 +1,37 @@
+--[[
+    simple keyword generator for youtube
+]]
+
+local v1 = {
+    "Escape From Tarkov",
+    "EFT",
+    "Tarkov",
+}
+
+local v2 = {
+    "Cheats",
+    "Gameplay",
+    "Hacks",
+    "Money",
+}
+
+local v3 = (function()
+    local _v1 = [[
+        keywords: 
+    ]]
+    local _v2 = _v1
+    for k, v in pairs(v1) do
+        for _k, _v in pairs(v2) do
+            local _v3 = (v.." ".._v)
+            if (_v1 == _v2) then
+                _v1 = (_v1.._v3)
+            else
+                _v1 = (_v1..", ".._v3)
+            end
+        end
+    end
+
+    print(_v1)
+end)
+
+v3()
